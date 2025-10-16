081544a0a9c0dd795b55dc6016c8e2c8be424a7e
Muhammad Talha Bilal <talha.infiniti@gmail.com>
2025-10-16 22:11:40 +0300

    Update Home.jsx

--- src/pages/frontend/Home.jsx
+++ src/pages/frontend/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
     // Fetch dishes from Flask API
     useEffect(() => {
         axios
-            .get("http://localhost:5000/api/fdishes")
+            .get("https://foodiebackend-1-ef18.onrender.com/api/fdishes")
             .then((res) => {
                 setDishes(res.data.dishes); // access dishes array
                 setToday(res.data.today);   // access today name
