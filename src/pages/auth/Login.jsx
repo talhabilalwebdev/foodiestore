c8b8d246d29298ce5dbd1c99023273862401a35a
Muhammad Talha Bilal <talha.infiniti@gmail.com>
2025-10-17 02:12:07 +0300

    Update Login.jsx

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -22,7 +22,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("https://foodiebackend-ru76.onrender.com/api/login", {
+      const res = await fetch("https://foodiebackend-1-ef18.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
