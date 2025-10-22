0242f108add54b2781e3854802a5b221a0be7c6e
Muhammad Talha Bilal <talha.infiniti@gmail.com>
2025-10-23 01:28:44 +0300

    Update CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -33,7 +33,7 @@ export function CartProvider({ children }) {
     const today = new Date().toLocaleDateString("en-US", { weekday: "long" });
 
     if (dish.day?.toLowerCase() !== today.toLowerCase()) {
-      toast.error("You can only order today's dishes!");
+      toast.error(`You can only order today's dishes! ${today}`);
       return;
     }
 
